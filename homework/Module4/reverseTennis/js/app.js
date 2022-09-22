10ea2f9a9fff130bc966708d16a43f0de7081636
Jesús <andradejesus@outlook.com>
2022-09-22 16:32:57 -0400

    Update app.js

--- homework/Module4/reverseTennis/js/app.js
+++ homework/Module4/reverseTennis/js/app.js
@@ -2,9 +2,10 @@
 //Jesús Andrade
 //FALL N220
  
+//objects containing paddles
  let lPaddle = {x: 10, y: 175, w: 20, h: 50};
 let rPaddle = {x: 770, y: 175, w: 20, h: 50};
-
+//controls the speed at which the paddles move
 let speed = 5;
 
 function setup() {
@@ -14,13 +15,15 @@ function setup() {
 function draw() {
     background(80, 255, 100);
     noStroke();
-    fill(255);
-    rect(lPaddle.x, lPaddle.y, lPaddle.w, lPaddle.h);
-    rect(rPaddle.x, rPaddle.y, rPaddle.w, rPaddle.h);
-    if (keyIsDown(DOWN_ARROW)) {
+    fill(255); //paddle color
+    rect(lPaddle.x, lPaddle.y, lPaddle.w, lPaddle.h); //left paddle
+    rect(rPaddle.x, rPaddle.y, rPaddle.w, rPaddle.h); //right paddle
+    //if statement controlling paddles, pressing down arrow makes left paddle move down and right paddle move up 
+    if (keyIsDown(DOWN_ARROW)) {  
         lPaddle.y += speed;
         rPaddle.y -= speed;
     }
+    //function controlling paddles, pressing up arrow makes left paddle move up and right paddle move down 
     if (keyIsDown(UP_ARROW)) {
         lPaddle.y -= speed;
         rPaddle.y += speed;
