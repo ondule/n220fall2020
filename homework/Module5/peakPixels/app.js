a0dff6ed81ba94dc67e2d7f0d1cd78176dd88860
Jesús <andradejesus@outlook.com>
2022-09-29 17:34:37 -0400

    Module 5 Assignments

---
+++ homework/Module5/peakPixels/app.js
@@ -0,0 +1,14 @@
+// let size = {
+//     x: "100px",
+//     y: "100px"
+// }
+
+// var div = document.createElement("div");
+// div.style.width = size.x;
+// div.style.height = size.y;
+// div.innerHTML = "PEAK PIXELS";
+// div.style.background = "green";
+// document.body.appendChild(div);
+// div.onclick = function changeColor() {
+//     size * 1.1;
+// }
